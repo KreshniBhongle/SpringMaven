516047a6fee9e041c4fc8ff1cc7e81785d79cc48
KreshniBhongle <kreshnibhongle1598@gmail.com>
2023-11-04 22:47:32 +0530

    This is first maven project

---
+++ src/main/java/com/AdditionalLogic1/calculator/Sum.java
@@ -0,0 +1,11 @@
+package com.AdditionalLogic1.calculator;
+
+public class Sum {
+
+	public int sum(int a, int b) {
+		int c= a+b;
+		System.out.println("Addition of a and b is "+c);
+		return c;
+		
+	}
+}
---
+++ src/test/java/com/AdditionalLogic1/AdditionalLogic1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdditionalLogic1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditionalLogic1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
