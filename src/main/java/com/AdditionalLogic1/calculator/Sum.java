f8bb073ac14a889d2e76d1a5d57b3d32b788d4b1
KreshniBhongle <kreshnibhongle1598@gmail.com>
2023-11-05 01:06:32 +0530

    Commit by kresh

--- src/main/java/com/AdditionalLogic1/calculator/Sum.java
+++ src/main/java/com/AdditionalLogic1/calculator/Sum.java
@@ -19,9 +19,30 @@ public class Sum {
 	//imp work
 	public int Div(int a, int b) {
 		int c=a/b;
+		return c;
+	}
+	
+	public int Test1(int a, int b) {
+		int c=a-b;
+		System.out.println(c);
+		return c;
+		
+	}
+
+	public int Test2(int a, int b) {
+		int c=a-b;
+
 		System.out.println(c);
 		return c;
 		
 	}
 	
+	public int Test3(int a, int b) {
+		int c=a-b;
+		System.out.println(c);
+		return c;
+		
+	}
+
+
 }
